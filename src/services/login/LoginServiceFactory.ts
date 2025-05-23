7eef1007c1a55cb560b862bd32cd20b05e04bea6
Sanjay Makvana <Timglp@1618>
2025-05-23 21:15:03 +0100

    WU-1 test

--- src/services/login/LoginServiceFactory.ts
+++ src/services/login/LoginServiceFactory.ts
@@ -5,6 +5,7 @@ import { LoginService } from './LoginService'; // Interface
 export const LoginServiceFactory = (): LoginService => {
     console.log('Using service:', import.meta.env.VITE_USE_MOCK); // Log the environment value
     console.log('import.meta.env.VITE_USE_MOCK:', import.meta.env.VITE_USE_MOCK); // Log the environment value
+    console.log('Testing.......');
     if (import.meta.env.VITE_USE_MOCK === 'true') {
         console.log('Using Mock Service');
         return new LoginServiceMockImpl(); // Mock service
